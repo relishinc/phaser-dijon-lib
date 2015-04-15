35b77b8a132c147fbebf950a27485d0ed6cc83bd
Anthony Sapp <anthonysapp@gmail.com>
2015-04-15 17:32:26 -0400

    changed CopyModel retrieval of text from node to accommodate weird case where line breaks in XML causes the DOMParser to see 3 child nodes

--- model/CopyModel.js
+++ model/CopyModel.js
@@ -1,37 +1,39 @@
-var CopyModel = function (game, xml) {
+var CopyModel = function(game, xml) {
     //make sure only one copy model is attached to the game
-    if (game && typeof game.copy !== 'undefined'){
+    if (game && typeof game.copy !== 'undefined') {
         return false;
     }
 
     game.copy = this;
 
-    if (typeof xml !== 'undefined'){
+    if (typeof xml !== 'undefined') {
         this.setXML(xml);
     }
 };
 
 CopyModel.prototype.constructor = CopyModel;
 
-CopyModel.prototype.getCopy = function (groupId, itemId) {
+CopyModel.prototype.getCopy = function(groupId, itemId) {
     if (!this.hasXML()) {
         return false;
     }
     var node = this.xml.querySelector('[id="' + groupId + '"] [id="' + itemId + '"]');
-    return node ? this.replaceChars(node.childNodes[0].nodeValue) : null;
+
+    // fixes a case where line breaks are counted as text nodes in the domParser
+    return node ? this.replaceChars(node.childNodes.length > 1 ? node.childNodes[0].wholeText.trim() : node.childNodes[0].nodeValue) : null;
 };
 
-CopyModel.prototype.replaceChars = function(text){
-    var result = text.replace(/\\n/g,'\n');
+CopyModel.prototype.replaceChars = function(text) {
+    var result = text.replace(/\\n/g, '\n');
     return result;
 };
 
-CopyModel.prototype.setXML = function (xml) {
+CopyModel.prototype.setXML = function(xml) {
     var parser = new DOMParser();
     this.xml = parser.parseFromString(xml, "application/xml");
 };
 
-CopyModel.prototype.hasXML = function () {
+CopyModel.prototype.hasXML = function() {
     return typeof this.xml !== 'undefined';
 };
 
