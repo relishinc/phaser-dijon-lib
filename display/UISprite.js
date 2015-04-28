2e96beea0cd398a634c97e09bc69e696f15c9801
Anthony Sapp <anthonysapp@gmail.com>
2015-04-28 17:14:51 -0400

    removed unneeded debug parameters

--- display/UISprite.js
+++ display/UISprite.js
@@ -2,7 +2,6 @@ var UISprite = function(game, x, y, key, frame, name, autoAdd) {
 
     Phaser.Sprite.call(this, game, x, y, key, frame);
     this.__frameName = frame;
-    this.debug = true;
 
     this.name = typeof name === 'undefined' ? typeof frame !== 'undefined' ? frame : 'UISprite' : name;
 
@@ -12,11 +11,6 @@ var UISprite = function(game, x, y, key, frame, name, autoAdd) {
 
     this.init();
     this.buildInterface();
-
-    if (this.game.debugMode && this.debug) {
-        this.inputEnabled = true;
-        this.addDebugSettings();
-    }
 };
 
 UISprite.prototype = Object.create(Phaser.Sprite.prototype);
@@ -30,28 +24,19 @@ UISprite.prototype.buildInterface = function() {
     // add visual elements
 };
 
-UISprite.prototype.addDebugSettings = function() {
-
-    this.events.onInputDown.add(this.handleClick, this);
-    this.input.enableDrag();
-    this.game.debugList.push(this);
-
-};
-
 UISprite.prototype.getFramePrefix = function() {
-    var frameArr;
+    var frameArr,
+        prefix;
     if (typeof(this.__frameName) !== 'string') {
         return '';
     }
     if (this.__frameName.indexOf('/')) {
         frameArr = this.__frameName.split('/');
-        return frameArr[0];
+        frameArr.pop();
+        prefix = frameArr.join('/');
+        return prefix;
     }
     return this.__frameName;
-};
-
-UISprite.prototype.handleClick = function() {
-    this.game.debugItem = this;
 };
 
 UISprite.prototype.hide = function() {
@@ -64,10 +49,6 @@ UISprite.prototype.addAnim = function(animName, animPath, numFrames) {
 
 UISprite.prototype.playAnim = function(animName, framerate, loop, killOnComplete) {
     this.animations.play(animName, framerate, loop, killOnComplete);
-};
-
-UISprite.prototype.centerPivot = function() {
-    this.pivot.set(this.width >> 1, this.height >> 1);
 };
 
 UISprite.prototype.centerAnchor = function() {
