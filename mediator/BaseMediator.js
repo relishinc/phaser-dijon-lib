a809c9d836fd670cf5def433ff1d08d0a3313b4d
Anthony Sapp <anthonysapp@gmail.com>
2015-05-01 17:12:42 -0400

    added BaseMediator added setColor method to UIText

---
+++ mediator/BaseMediator.js
@@ -0,0 +1,46 @@
+var BaseMediator = function (game, mediatorName, viewComponent, modelClass, autoReg) {
+  this.game = game;
+
+  if (autoReg !== false)
+    this.game[mediatorName] = this;
+
+  if (typeof viewComponent !== 'undefined') {
+    this.setViewComponent(viewComponent);
+  }
+
+  if (typeof modelClass !== 'undefined') {
+    this._createModel(modelClass);
+  }
+
+};
+
+BaseMediator.prototype.constructor = BaseMediator;
+
+// private methods
+BaseMediator.prototype._createModel = function (ModelClass) {
+  this.setModel(new ModelClass(this.game));
+};
+
+// public methods
+BaseMediator.prototype.createSignals = function () {
+  _.each(arguments, this.addSignal, this);
+};
+
+BaseMediator.prototype.addSignal = function (signalName) {
+  this[signalName] = new Phaser.Signal();
+};
+
+BaseMediator.prototype.setViewComponent = function (viewComponent) {
+  this.viewComponent = viewComponent;
+};
+
+BaseMediator.prototype.setModel = function (model) {
+  this._model = model;
+};
+
+BaseMediator.prototype.getCopy = function (group, id) {
+  if (typeof this.game.copy !== 'undefined')
+    return this.game.copy.getCopy(group, id);
+};
+
+module.exports = BaseMediator;
--- model/CopyModel.js
+++ model/CopyModel.js
@@ -1,42 +1,40 @@
-var CopyModel = function(game, xml) {
-    //make sure only one copy model is attached to the game
-    if (game && typeof game.copy !== 'undefined') {
-        return false;
-    }
+var CopyModel = function (game, xml) {
+  //make sure only one copy model is attached to the game
+  if (game && typeof game.copy !== 'undefined') {
+    return false;
+  }
 
-    game.copy = this;
+  game.copy = this;
 
-    if (typeof xml !== 'undefined') {
-        this.setXML(xml);
-    }
+  if (typeof xml !== 'undefined') {
+    this.setXML(xml);
+  }
 };
 
 CopyModel.prototype.constructor = CopyModel;
 
-CopyModel.prototype.getCopy = function(groupId, itemId) {
-    if (!this.hasXML()) {
-        return false;
-    }
-    var node = this.xml.querySelector('[id="' + groupId + '"] [id="' + itemId + '"]');
+CopyModel.prototype.getCopy = function (groupId, itemId) {
+  if (!this.hasXML()) {
+    return false;
+  }
+  var node = this.xml.querySelector('[id="' + groupId + '"] [id="' + itemId + '"]');
 
-    // fixes a case where line breaks are counted as text nodes in the domParser
-    return node ? this.replaceChars(node.childNodes.length > 1 ? node.childNodes[0].wholeText.trim() : node.childNodes[0].nodeValue) : null;
+  // fixes a case where line breaks are counted as text nodes in the domParser
+  return node ? this.replaceChars(node.childNodes.length > 1 ? node.childNodes[0].wholeText.trim() : node.childNodes[0].nodeValue) : null;
 };
 
-CopyModel.prototype.replaceChars = function(text) {
-    var result = text.replace(/\\n/g, '\n');
-    return result;
+CopyModel.prototype.replaceChars = function (text) {
+  var result = text.replace(/\\n/g, '\n');
+  return result;
 };
 
-CopyModel.prototype.setXML = function(xml) {
-    var parser = new DOMParser();
-    this.xml = parser.parseFromString(xml, "application/xml");
+CopyModel.prototype.setXML = function (xml) {
+  var parser = new DOMParser();
+  this.xml = parser.parseFromString(xml, "application/xml");
 };
 
-CopyModel.prototype.hasXML = function() {
-    return typeof this.xml !== 'undefined';
+CopyModel.prototype.hasXML = function () {
+  return typeof this.xml !== 'undefined';
 };
-
-CopyModel.intilialized = false;
 
 module.exports = CopyModel;
