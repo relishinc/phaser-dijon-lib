0f6376677bcb764e210474b8009eef002a93be6c
Anthony Sapp <anthonysapp@gmail.com>
2015-09-08 08:43:38 -0400

    no message

--- build/dijon.js
+++ build/dijon.js
@@ -909,7 +909,8 @@ var dijon;
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    __.prototype = b.prototype;
+    d.prototype = new __();
 };
 var dijon;
 (function (dijon) {
